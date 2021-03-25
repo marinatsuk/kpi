44d073d7b33c8c7e62c166319534491ed26ab9ad
marinatsuk <73252923+marinatsuk@users.noreply.github.com>
2021-03-25 11:51:56 +0200

    Create Node.java

---
+++ term2/ADS/lab2/Node.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    Node(Integer value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+}
